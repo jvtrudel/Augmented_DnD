f766d98febd571f4539db8f4c8dc9777d8bbc7e9
Jérémy Viau-Trudel <jeremy.vtrudel@gmail.com>
2019-12-29 23:47:14 -0500

    minimal exemple de communication bi-directionelle

--- server/index.js
+++ server/index.js
@@ -5,14 +5,17 @@ var ws = require('./ws')
 var app = express()
 
 app.get('/', function (req, res) {
-   res.sendfile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/index.html');
 })
 
 app.set('trust proxy',
-function (ip) {
-  if (ip === '127.0.0.1' || ip === '192.168.0.149') return true // trusted IPs
-  else return false
+   function (ip) {
+      if (ip === '127.0.0.1' || ip === '192.168.0.149') return true // trusted IPs
+      else return false
+   }
 )
+
 app.listen(3000, function () {
    console.log('Example app listening on port 3000!')
 })
+
--- server/ws.js
+++ server/ws.js
@@ -2,6 +2,12 @@ var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function connection (ws) {
-    console.log('received: %s', message)
+    console.log('received: %s', "hello from websocket")
+    ws.on('message', function incoming(message) {
+      console.log('received message: %s', message);
+    });
+    
     ws.send(`${new Date()}`)
 })
+
+
